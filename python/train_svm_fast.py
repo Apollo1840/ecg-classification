d47a53bd2174afb88fce68e67403c32513e9e1cb
Apollo1840 <zoucongyu1993@hotmail.com>
2020-06-04 22:47:46 +0200

    quick debug

--- python/train_svm_fast.py
+++ python/train_svm_fast.py
@@ -6,6 +6,7 @@ import joblib
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn import svm
+from sklearn.metrics import f1_score
 from pprint import pprint
 
 from util_path_manager import path_to_model, path_to_measure
@@ -14,24 +15,46 @@ from model_aggregation import *
 from utils import PrintTime, calc_class_weights
 
 
+def model_search_unit():
+    fixed_parameters = {
+        "multi_mode": "ovo",
+        "winL": 90,
+        "winR": 90,
+        "do_preprocess": True,
+        "use_weight_class": True,
+        "maxRR": True,
+        "use_RR": False,
+        "norm_RR": True,
+        "oversamp_method": "",
+        "cross_patient": False,
+        "reduced_DS": False,
+        "verbose": True,
+    }
+
+    searchable_params = {
+        "c_value": 0.001,
+        "gamma_value": 0.0,
+        "compute_morph": ['resample_10', 'lbp', 'hbf5', 'wvlt', 'HOS'],
+    }
+
+    train_and_evaluation(**fixed_parameters, **searchable_params)
+
+
 def train_and_evaluation(
         multi_mode='ovo',
         winL=90,
         winR=90,
         do_preprocess=True,
-        use_weight_class=False,
+        use_weight_class=True,
         maxRR=True,
-        use_RR=True,
+        use_RR=False,
         norm_RR=True,
         compute_morph={''},
         oversamp_method='',
-        pca_k=0,
-        feature_selection=False,
         cross_patient=False,
         c_value=0.001,
         gamma_value=0.0,
         reduced_DS=False,
-        leads_flag=[1, 0],
         verbose=True,
 ):
     """
@@ -49,8 +72,6 @@ def train_and_evaluation(
     :param norm_RR:
     :param compute_morph:
     :param oversamp_method:
-    :param pca_k: int, if it is larger than 0, PCA will be activated
-    :param feature_selection: Bool,
     :param cross_patient: Bool
     :param c_value:
     :param gamma_value:
@@ -94,19 +115,12 @@ def train_and_evaluation(
     if verbose:
         print("Testing model on MIT-BIH DS2: " + model_svm_path + "...")
 
-    perf_measures_path = path_to_measure(**params_for_naming)
-    eval_model_module(perf_measures_path,
-                      svm_model,
-                      tr_features,
-                      tr_labels,
-                      eval_features,
-                      eval_labels,
-                      **model_kwargs,
-                      verbose=verbose)
+    pred_labels = svm_model.predict(eval_features)
+    print("marco f1 score: ", f1_score(eval_labels, pred_labels, average='macro'))
 
     print("congrats! evaluation complete! ")
 
-    return svm_model, eval_features, eval_labels
+    return svm_model, pred_labels, eval_labels
 
 
 def load_ml_data(ws,
@@ -163,7 +177,8 @@ def load_ml_data(ws,
                                           ml_norm_RR,
                                           ml_compute_morph)
 
-        tr_features, eval_features, tr_labels, eval_labels = train_test_split(features, labels,
+        tr_features, eval_features, tr_labels, eval_labels = train_test_split(features,
+                                                                              labels,
                                                                               test_size=0.2,
                                                                               random_state=2020)
 
@@ -185,7 +200,11 @@ def load_ml_data(ws,
     return tr_features_scaled, tr_labels, eval_features_scaled, eval_labels
 
 
-def train_model_module(model_svm_path, tr_features, tr_labels, verbose=False, **model_kwargs):
+def train_model_module(model_svm_path,
+                       tr_features,
+                       tr_labels,
+                       verbose=False,
+                       **model_kwargs):
     """
     train the model if it is needed.
 
@@ -205,10 +224,16 @@ def train_model_module(model_svm_path, tr_features, tr_labels, verbose=False, **
     return svm_model
 
 
-def train_model(model_svm_path, tr_features, tr_labels, verbose=False, **model_kwargs):
+def train_model(model_svm_path,
+                tr_features,
+                tr_labels,
+                verbose=False,
+                **model_kwargs):
+
     C_value = model_kwargs.get("c_value", 1)
     gamma_value = model_kwargs.get("gamma_value", 0)
     multi_mode = model_kwargs.get("multi_model", "ovo")
+
     use_probability = False
     class_weights = calc_class_weights(tr_labels)
     # class_weight='balanced',
@@ -217,10 +242,20 @@ def train_model(model_svm_path, tr_features, tr_labels, verbose=False, **model_k
     # TODO load best params from cross validation!
 
     # NOTE 0.0 means 1/n_features default value
-    svm_model = svm.SVC(C=C_value, kernel='rbf', degree=3, gamma=gamma_value,
-                        coef0=0.0, shrinking=True, probability=use_probability, tol=0.001,
-                        cache_size=200, class_weight=class_weights, verbose=verbose,
-                        max_iter=-1, decision_function_shape=multi_mode, random_state=None)
+    svm_model = svm.SVC(C=C_value,
+                        kernel='rbf',
+                        degree=3,
+                        gamma=gamma_value,
+                        coef0=0.0,
+                        shrinking=True,
+                        probability=use_probability,
+                        tol=0.001,
+                        cache_size=200,
+                        class_weight=class_weights,
+                        verbose=verbose,
+                        max_iter=-1,
+                        decision_function_shape=multi_mode,
+                        random_state=None)
 
     with PrintTime("train the model", verbose=verbose):
         svm_model.fit(tr_features, tr_labels)
@@ -234,138 +269,6 @@ def train_model(model_svm_path, tr_features, tr_labels, verbose=False, **model_k
     # TODO Export StandardScaler()
 
     return svm_model
-
-
-def eval_model_module(perf_measures_path,
-                      svm_model,
-                      tr_features,
-                      tr_labels,
-                      eval_features,
-                      eval_labels,
-                      multi_mode,
-                      c_value,
-                      gamma_value,
-                      is_include_train=False,
-                      is_include_ovo_voting_exp=False,
-                      verbose=False,
-                      **kwargs
-                      ):
-    # ovo_voting:
-    # Let's test new data!
-    print("Evaluation on DS2 ...")
-    eval_model(svm_model,
-               eval_features,
-               eval_labels,
-               multi_mode,
-               'ovo_voting',
-               perf_measures_path,
-               c_value,
-               gamma_value,
-               verbose=verbose,
-               DS='')
-
-    # Simply add 1 to the win class
-    if is_include_train:
-        print("Evaluation on DS1 ...")
-        eval_model(svm_model,
-                   tr_features,
-                   tr_labels,
-                   multi_mode,
-                   'ovo_voting',
-                   perf_measures_path,
-                   c_value,
-                   gamma_value,
-                   verbose=verbose,
-                   DS='Train_')
-
-    # ovo_voting_exp:
-    # Consider the post prob adding to both classes
-    if is_include_ovo_voting_exp:
-        # Let's test new data!
-        print("Evaluation on DS2 ...")
-        eval_model(svm_model,
-                   eval_features,
-                   eval_labels,
-                   multi_mode,
-                   'ovo_voting_exp',
-                   perf_measures_path,
-                   c_value,
-                   gamma_value,
-                   verbose=verbose,
-                   DS='')
-
-        if is_include_train:
-            print("Evaluation on DS1 ...")
-            eval_model(svm_model,
-                       tr_features,
-                       tr_labels,
-                       multi_mode,
-                       'ovo_voting_exp',
-                       perf_measures_path,
-                       c_value,
-                       gamma_value,
-                       verbose=verbose,
-                       DS='Train_')
-
-
-# Eval the SVM model and export the results
-def eval_model(svm_model, features, labels, multi_mode, voting_strategy,
-               output_path, C_value, gamma_value, DS, verbose=False):
-    """
-
-    :param svm_model:
-    :param features:
-    :param labels:
-    :param multi_mode: str, 'ovo' or 'ovr'
-    :param voting_strategy: str, has effect when multi_model is ovo
-    :param output_path:
-    :param C_value:
-    :param gamma_value:
-    :param DS: Str, "" or "Train_", basically used as a marker in path name.
-    :return:
-    """
-
-    decision = svm_model.decision_function(features)
-
-    if verbose:
-        print("make evaluation predict...")
-
-    if multi_mode == 'ovr':
-        predict = svm_model.predict(features)
-
-    else:  # elif multi_mode == 'ovo':
-        predict, _ = ovo_voting_handler(decision, 4, voting_strategy)
-
-    # record the performance
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
-
-    # svm_model.predict_log_proba  svm_model.predict_proba   svm_model.predict ...
-    perf_measures = compute_AAMI_performance_measures(predict, labels, verbose=verbose)
-    pprint(perf_measures.__dict__, indent=2)
-
-    # save results
-    gamma_str = 'g_' + str(gamma_value) if gamma_value != 0.0 else ""
-
-    # 1) save AAMI results
-    path_to_AAMI_results = os.path.join(output_path,
-                                        "{}C_{}{}_score_Ijk_{}_{}.txt".format(DS,
-                                                                              C_value,
-                                                                              gamma_str,
-                                                                              format(perf_measures.Ijk, '.2f'),
-                                                                              voting_strategy))
-    write_AAMI_results(perf_measures, path_to_AAMI_results)
-
-    # 2) save decsion
-    path_to_decision = os.path.join(output_path, "{}C_{}{}_decision_ovo.csv".format(DS, C_value, gamma_str))
-    np.savetxt(path_to_decision, decision)
-
-    # 3) save predict
-    path_to_predict = os.path.join(output_path, "{}C_{}_predict_{}.csv").format(DS, C_value, gamma_value,
-                                                                                voting_strategy)
-    np.savetxt(path_to_predict, predict.astype(int), '%.0f')
-
-    print("Results writed at " + output_path + '/' + DS + 'C_' + str(C_value))
 
 
 if __name__ == "__main__":
